2b0ac1a1ba1461fbd8385a802ebb817b0eaa1011
AdrianAlcolea <adrianalcoleamoreno@gmail.com>
2017-02-20 17:10:58 +0100

    hola chicooos!!!!

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ file_out = "data/out.txt"
 
 # N x M (char, n slice), 0
 global Matrix
-# Slice Array, index = n slice in matrix
+# Slice Array, index = n slice in Matrix
 global Slices
 global Solution
 global Row
@@ -21,11 +21,11 @@ global Max
 #Functions
 def read(file_name):
     f = open(file_name, 'r')
-    global Row, Col, Min, Max, matrix
+    global Row, Col, Min, Max, Matrix
     Row, Col, Min, Max = f.readline().strip().split()
     Row, Col, Min, Max = int(Row), int(Col), int(Min), int(Max)
     #Comprehension list, magia oscura
-    matrix = [[(ch , 0) for ch in line.strip()] for line in f]
+    Matrix = [[(ch , 0) for ch in line.strip()] for line in f]
 
 def write(file_name, Solution):
     f = open(file_name, 'w')
@@ -43,17 +43,25 @@ def guardar_mejor():
     print "a"
 
 def run():
-    global matrix, Solution
+    global Row, Col, Matrix, Solution
+    r1, c1, r2, c2 = 0
+    maxCol
 
-    Solution = "3\n" \
-               "0 0 2 1\n" \
-               "0 2 2 2\n" \
-               "0 3 2 4\n"
     read(file_in)
 
-    #
+    while (r1 >= 0 and r2 >= 0):
+        maxCol = Col
+        for r2 in range(r1, Row):
+            for c2 in range(r2, maxCol):
+                if maximoAlcanzado(r1, c1, r2, c2):
+                    break
+                if estaEnTrozo(r2, c2):
+                    maxCol = c2 - 1
+                    break
 
-    write (file_out,Solution)
+        r1, r2 = siguiente_inicio(r1, r2) # Devuelve -1, -1 si se sale
+
+    write(file_out, Solution)
 
 if __name__ == '__main__':
     run()
