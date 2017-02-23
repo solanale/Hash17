26ed71300a2db17fdf322147e8f2cfcf407238bf
Alex <asolanas93@gmail.com>
2017-02-23 19:48:16 +0100

    mapping done

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-#!/usr/bin/python
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
 #Files
 file_in = "data/me_at_the_zoo.in"
@@ -7,41 +8,65 @@ file_out = "data/out.txt"
 #Global vars
 
 global NVIDEOS, NENDPOINTS, NPETICIONES, NSERVERS, CAPACIDAD
-global Videos, Endpoints, Peticiones
+global Videos, Endpoints, Peticiones, Caches
 
 
 #Functions
 def read(file_name):
     f = open(file_name, 'r')
+
     global NVIDEOS, NENDPOINTS, NPETICIONES, NSERVERS, CAPACIDAD
     global Videos, Endpoints, Peticiones
     NVIDEOS, NENDPOINTS, NPETICIONES, NSERVERS, CAPACIDAD = f.readline().strip().split()
     print NVIDEOS, NENDPOINTS, NPETICIONES, NSERVERS, CAPACIDAD
     Videos = f.readline().strip().split()
+    print "Videos"
     print Videos
+
     Endpoints = []
+    Peticiones = []
+    Caches = []
+    for x in range(0,int(NSERVERS)):
+        Caches.append([])
     for x in range(0,int(NENDPOINTS)):
         Ld, K = f.readline().strip().split()
         Endpoints.append((Ld, []))
         for y in range (0, int(K)):
             (_, array) = Endpoints[x]
-            array.append(f.readline().strip().split())
+            aux = f.readline().strip().split()
+            array.append(aux)
+            Caches[int(aux[0])].append((x,aux[1]))
+
+        Peticiones.append(0)
+    print "Endpoints"
     print Endpoints
-    Peticiones = []
+    print "Caches"
+    print Caches
+
     for x in range (0, int(NPETICIONES)):
         Rv, Re, Rn = f.readline().strip().split()
-        Peticiones.append((Rv, Re, Rn))
+        Peticiones[int(Re)] = (Rv, Rn)
+    print "Peticiones"
     print Peticiones
+
     #Comprehension list, magia oscura
     # Matrix = [[(ch , 0) for ch in line.strip()] for line in f]
 
 def write(file_name):
-    global Slices
+
     f = open(file_name, 'w')
-    f.write(str(len(Slices)) + "\n")
-    for slice in Slices:
-        (n1,n2,n3,n4) = slice
-        f.write(str(n1)+" "+str(n2)+" "+str(n3)+" "+str(n4)+"\n")
+
+    #f write len del array
+
+    #f write
+
+
+
+
+    # f.write(str(len(Slices)) + "\n")
+    # for slice in Slices:
+    #     (n1,n2,n3,n4) = slice
+    #     f.write(str(n1)+" "+str(n2)+" "+str(n3)+" "+str(n4)+"\n")
 
 def run():
 
