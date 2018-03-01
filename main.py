ce22ded26360398d1fdfd8bc891e5be6e9c3ea90
AdrianAlcolea <adrianalcoleamoreno@gmail.com>
2018-03-01 19:26:29 +0100

    data

--- main.py
+++ main.py
@@ -12,44 +12,30 @@ file_in = "data/a_example.in"
 
 file_out = "data/out.txt"
 
-#Global vars
-
-global NVIDEOS, NENDPOINTS, NPETICIONES, NSERVERS, CAPACIDAD
-global Videos, Endpoints, Peticiones, Caches, Ganancias
-global Solucion, Final
-
-
 #Functions
 def read(file_name):
     
     with open(file_name, 'r') as f:
         
-        data = [[int(d) for d in l.split()] for l in f]
+        data = np.array([[int(d) for d in l.split()] for l in f])
     
     return data
-    
-def write(file_name):
-    global Final
-    f = open(file_name, 'w')
-    f.write(str(Final.pop(0)) + "\n")
-    for line in Final:
-        f.write(str(line[0]) + " " + " ".join(str(x) for x in line[1]) + "\n")
 
 
-def formatSolucion():
-    global Solucion, Final
-    Final = [0]
-    for x in range(0, len(Solucion)):
-        if len(Solucion[x]) > 0:
-            Final.append([x, Solucion[x]])
-    Final[0] = len(Final)-1
 
 def main():
 
     # Leer fichero
     data = read(file_in)
     
-    print(data)
+    R, C, F, N, B, T = data[0]
+    print(R, C, F, N, B, T)
+    
+    #for ride in range(1, data.shape[0]):
+        
+    #print(data)
+    
+    
 
 if __name__ == '__main__':
     main()
